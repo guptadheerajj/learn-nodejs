fc85a0fefe9cf30e2266ce6f4de41c531f037e0a
guptadheerajj <174727182+guptadheerajj@users.noreply.github.com>
2025-07-18 18:59:07 +0530

    learn more methods of fs core module

--- core_modules/fs_demo.js
+++ core_modules/fs_demo.js
@@ -4,41 +4,41 @@ const fsPromise = require("fs/promises");
 const { threadId } = require("worker_threads");
 
 // create folder
-// fs.mkdir(path.join(__dirname, "newFolder"), {}, (err) => {
-// 	if (err) throw new Error(err);
-// 	console.log("Directory created succsccfully");
-// });
+fs.mkdir(path.join(__dirname, "newFolder"), {}, (err) => {
+	if (err) throw new Error(err);
+	console.log("Directory created succsccfully");
+});
 
-// async function createFolder() {
-// 	try {
-// 		await fsPromise.mkdir(path.join(__dirname, "newFolder2"), {});
-// 		console.log("Directory created successfully");
-// 	} catch (err) {
-// 		console.log(err);
-// 	}
-// }
-// createFolder();
+async function createFolder() {
+	try {
+		await fsPromise.mkdir(path.join(__dirname, "newFolder2"), {});
+		console.log("Directory created successfully");
+	} catch (err) {
+		console.log(err);
+	}
+}
+createFolder();
 
 // Create and write to file
 
-// fs.writeFile(
-// 	path.join(__dirname, "/newFolder", "new.txt"),
-// 	"Hello World!",
-// 	(err) => {
-// 		if (err) console.log(err);
-// 		console.log("File written successfully!");
+fs.writeFile(
+	path.join(__dirname, "/newFolder", "new.txt"),
+	"Hello World!",
+	(err) => {
+		if (err) console.log(err);
+		console.log("File written successfully!");
 
-// 		// Append File
-// 		fs.appendFile(
-// 			path.join(__dirname, "/newFolder", "new.txt"),
-// 			" I love node.js!",
-// 			(err) => {
-// 				if (err) console.log(err);
-// 				console.log("File written successfully!");
-// 			}
-// 		);
-// 	}
-// );
+		// Append File
+		fs.appendFile(
+			path.join(__dirname, "/newFolder", "new.txt"),
+			" I love node.js!",
+			(err) => {
+				if (err) console.log(err);
+				console.log("File written successfully!");
+			}
+		);
+	}
+);
 
 // Read file
 fs.readFile(
@@ -59,3 +59,36 @@ fs.rename(
 		console.log("File renamed succseefully....");
 	}
 );
+
+// Task 1 -> add these names to a file and then display them on the console
+const users = [
+	{ name: "Kaddy" },
+	{ name: "Marc" },
+	{ name: "Prince" },
+	{ name: "Kally" },
+];
+
+fs.writeFile(
+	path.join(__dirname, "user-data.json"),
+	JSON.stringify(users),
+	(err) => {
+		if (err) throw err;
+		console.log("File written....");
+	}
+);
+
+fs.readFile("./user-data.json", "utf8", (err, data) => {
+	if (err) throw err;
+	const userData = JSON.parse(data);
+	console.log(userData);
+});
+
+// Read directory content
+fs.readdir(__dirname, (err, files) => {
+	if (err) throw err;
+
+	console.log("Files: ");
+	files.forEach((file) => {
+		console.log(file);
+	});
+});
